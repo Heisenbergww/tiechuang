6d9b981f8fd15fa0aa459ed06a410a37a96b8b80
junfeng <junfeng@junfengdeMBP.lan>
2016-09-27 19:11:41 +0800

    新数据库

--- oceanmin/config/db.php
+++ oceanmin/config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-	'dsn' => 'mysql:host=198.199.107.185;dbname=oceanmin',
+	'dsn' => 'mysql:host=198.199.107.185;dbname=shelf',
 	'username' => 'junfeng',
 	'password' => 'bike.com',
 	'charset' => 'utf8',
