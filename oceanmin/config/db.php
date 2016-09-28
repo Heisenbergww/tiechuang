6c854b329722a922fc912d3ab8bfe91c7d0c4685
junfeng <junfeng@junfengdeMBP.lan>
2016-09-28 15:05:12 +0800

    线上模式

--- oceanmin/config/db.php
+++ oceanmin/config/db.php
@@ -3,7 +3,7 @@
 return [
     'class' => 'yii\db\Connection',
 	'dsn' => 'mysql:host=127.0.0.1;dbname=shelf',
-	'username' => 'junfeng',
+	'username' => 'root',
 	'password' => 'bike.com',
 	'charset' => 'utf8',
 	'tablePrefix' => 'ocean_',
