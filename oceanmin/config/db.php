3c198db42d30bf00c02e4e2a447f8449d867dba5
junfeng <junfeng@junfengdeMBP.lan>
2016-09-29 09:51:07 +0800

    SchemaCache

--- oceanmin/config/db.php
+++ oceanmin/config/db.php
@@ -7,4 +7,9 @@ return [
 	'password' => 'bike.com',
 	'charset' => 'utf8',
 	'tablePrefix' => 'ocean_',
+	'enableSchemaCache' => true,
+    // Duration of schema cache.
+    'schemaCacheDuration' => 3600,
+    // Name of the cache component used to store schema information
+    'schemaCache' => 'cache',
 	];
