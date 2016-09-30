76781f45c3e51470d0dd8c608d70c2aa60eb8c0a
Heisenbergww <804454576@qq.com>
2016-09-30 15:35:23 +0800

    home展示图片大小，展品页位置

--- oceanmin/config/db.php
+++ oceanmin/config/db.php
@@ -2,9 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-	'dsn' => 'mysql:host=127.0.0.1;dbname=shelf',
-	'username' => 'root',
-	'password' => 'bike.com',
-	'charset' => 'utf8',
-	'tablePrefix' => 'ocean_',
-	];
+      'dsn' => 'mysql:host=162.243.136.18;dbname=shelf',
+      'username' => 'junfeng',
+      'password' => 'bike.com',
+      'charset' => 'utf8',
+      'tablePrefix' => 'ocean_',
+      ];
